d0c2ad0644b45994b07329fac6531dbb37aa7b8b
Anthony Luo <anthonyluo100@gmail.com>
2022-10-15 12:59:57 -0400

    interrupt ISR sequence confirmed working
    can now see counter ticking up when we hit the interrupt. Turns out print statement stake too long

--- software/Lab1/hello_world.c
+++ software/Lab1/hello_world.c
@@ -3,6 +3,8 @@
 #include "sys/alt_irq.h"
 #include "altera_avalon_pio_regs.h"
 
+static interrupt_count = 0;
+
 int background() {
 	int j;
 	int x = 0;
@@ -16,26 +18,30 @@ int background() {
 
 static void stimulus_in_ISR(void* context, alt_u32 id) {
 	// Return to Response Out here
-
+	interrupt_count += 1;
 	// clear our interrupt bit here
 	IOWR(STIMULUS_IN_BASE, 3, 0x0);
 }
 
 int main()
 {
-	alt_irq_register(BUTTON_PIO_IRQ, (void *) 0, stimulus_in_ISR);
+	alt_irq_register(STIMULUS_IN_IRQ, (void *) 0, stimulus_in_ISR);
 
 	IOWR(STIMULUS_IN_BASE, 2, 0x1);
 
-	IOWR(EGM_BASE, 2, 0xffff);
-	IOWR(EGM_BASE, 3, 0xfff);
+	// max period 0xffff
+	int period = 1000;
+	int pulse_width = period / 2;
+	IOWR(EGM_BASE, 2, period);
+	IOWR(EGM_BASE, 3, pulse_width);
 
 	IOWR(EGM_BASE, 0, 1);
 	while(IORD(EGM_BASE, 1)) {
 		printf("looping \n");
+		background();
 	}
 	IOWR(EGM_BASE, 0, 0);
 
-	printf("done program \n");
+	printf("done program %d \n", interrupt_count);
 	return 0;
 }
