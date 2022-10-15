7b1fe2c5564b822b91ae68f7b17326acacc9cfa2
Anthony Luo <anthonyluo100@gmail.com>
2022-10-15 13:07:42 -0400

    avg_latency, missed, multi all looking good for interrupts

--- software/Lab1/hello_world.c
+++ software/Lab1/hello_world.c
@@ -19,6 +19,8 @@ int background() {
 static void stimulus_in_ISR(void* context, alt_u32 id) {
 	// Return to Response Out here
 	interrupt_count += 1;
+	IOWR(RESPONSE_OUT_BASE, 0, 1);
+	IOWR(RESPONSE_OUT_BASE, 0, 0);
 	// clear our interrupt bit here
 	IOWR(STIMULUS_IN_BASE, 3, 0x0);
 }
@@ -40,8 +42,15 @@ int main()
 		printf("looping \n");
 		background();
 	}
+
+	// get our data
+
+	int avg_latency = IORD(EGM_BASE, 4);
+	int missed = IORD(EGM_BASE, 5);
+	int multi = IORD(EGM_BASE, 6);
+
 	IOWR(EGM_BASE, 0, 0);
 
-	printf("done program %d \n", interrupt_count);
+	printf("done program %d with avg_latency: %d, missed: %d, multi: %d \n", interrupt_count, avg_latency, missed, multi);
 	return 0;
 }
